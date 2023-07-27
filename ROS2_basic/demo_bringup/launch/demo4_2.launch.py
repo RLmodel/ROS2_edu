0da728543d1259eb7d3e36c993229793572e7ece
ybbaek <ybbaek@rlmodel.com>
2023-07-28 08:36:25 +0900

    add demo4 with rename

---
+++ ROS2_basic/demo_bringup/launch/demo4_2.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+#from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python import get_package_share_directory
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    demo_launch_file = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('demo_bringup'),
+                         'launch/demoxml1.launch.xml')
+        )
+    )
+
+    ld.add_action(demo_launch_file)
+    return ld
