35852e1600e2eed2d0eddaf9ea78d4e4f4c83881
ybbaek <ybbaek@rlmodel.com>
2023-07-28 08:47:39 +0900

    rename python ver

---
+++ ROS2_basic/demo_bringup/launch/demopy4_3.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+#from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+#from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_yaml.launch_description_sources import YAMLLaunchDescriptionSource
+import os
+from ament_index_python import get_package_share_directory
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    demo_launch_file = IncludeLaunchDescription(
+        #PythonLaunchDescriptionSource(
+        #XMLLaunchDescriptionSource(
+        YAMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('demo_bringup'),
+                         'launch/demoyaml1.launch.yaml')
+        )
+    )
+
+    ld.add_action(demo_launch_file)
+    return ld
