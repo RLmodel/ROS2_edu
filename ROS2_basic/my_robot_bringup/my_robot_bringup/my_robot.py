18a1b9650e0384cbcf6f55e1aef8afbf5a104192
ybbaek <ybbaek@rlmodel.com>
2023-07-30 15:23:58 +0900

    repeat

--- ROS2_basic/my_robot_bringup/my_robot_bringup/my_robot.py
+++ ROS2_basic/my_robot_bringup/my_robot_bringup/my_robot.py
@@ -4,11 +4,13 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("first_node")
+        self.counter = 0
         self.get_logger().info("Hello RLmodel ROS2")
         self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self): # inside node
-        self.get_logger().info("hi log")
+        self.get_logger().info("hi log "+str(self.counter))
+        self.counter +=1
 
 def main(args=None):
     rclpy.init(args=args)
