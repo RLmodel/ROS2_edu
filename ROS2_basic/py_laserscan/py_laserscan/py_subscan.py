75fd9fd9b009c88cd0b6072634d1fcbf37765824
ybbaek <ybbaek@rlmodel.com>
2023-08-12 21:02:15 +0900

    add sub laser scan msg scan ranges index debug

--- ROS2_basic/py_laserscan/py_laserscan/py_subscan.py
+++ ROS2_basic/py_laserscan/py_laserscan/py_subscan.py
@@ -1,4 +1,3 @@
-import sys
 import rclpy
 from rclpy.node import Node
 
@@ -17,30 +16,19 @@ class ScanSub(Node):
 
     def listener_callback(self, msg):
         len0_left = format(msg.ranges[0], '.2f')
-        len720_right = format(msg.ranges[719], '.2f')
-        len360_front = format(msg.ranges[359], '.2f')
-        self.get_logger().info(f'{len0_left} {len720_right}  {len360_front}')
-        self.get_logger().info("Left Right Front")
+        len720_right = format(msg.ranges[720], '.2f')
+        len360_front = format(msg.ranges[360], '.2f')
+        self.get_logger().info(f'{len0_left} {len720_right} {len360_front}')
+        self.get_logger().info("Left / Right / Front")
+        print("Left / Right / Front")
 
 def main(args=None):
     rclpy.init(args=args)
     laser_sub = ScanSub()
-    '''
     rclpy.spin(laser_sub)
 
     laser_sub.destroy_node()
     rclpy.shutdown()
-    '''
-    try:
-        rclpy.spin(laser_sub)
-    except KeyboardInterrupt:
-        laser_sub.get_logger().info('==== Server stopped cleanly ====')
-    except BaseException:
-        laser_sub.get_logger().info('!! Exception in server:', file=sys.stderr)
-        raise
-    finally:
-        # (optional - Done automatically when node is garbage collected)
-        rclpy.shutdown()
 
 if __name__=='__main__':
     main()
