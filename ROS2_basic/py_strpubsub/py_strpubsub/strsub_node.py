24334535991fa2a88d0a6191a4b4dcf1b3798f99
ybbaek <ybbaek@rlmodel.com>
2023-12-03 20:57:55 +0900

    rename debug

--- ROS2_basic/py_strpubsub/py_strpubsub/strsub_node.py
+++ ROS2_basic/py_strpubsub/py_strpubsub/strsub_node.py
@@ -47,7 +47,7 @@ def main(args=None):
   # Destroy the node explicitly
   # (optional - otherwise it will be done automatically
   # when the garbage collector destroys the node object)
-  minimal_subscriber.destroy_node()
+  str_sub.destroy_node()
    
   # Shutdown the ROS client library for Python
   rclpy.shutdown()
